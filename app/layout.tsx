c6b9e9263a4e52df9be2e2608bfa1971d7d1c871
realwizz <womiowuna@gmail.com>
2026-05-09 22:17:29 +0100

    css updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,16 @@
+import { Geist } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
+
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={cn("font-sans", geist.variable)}>
       <body>
         <div className="flex">
           {/* Sidebar */}
